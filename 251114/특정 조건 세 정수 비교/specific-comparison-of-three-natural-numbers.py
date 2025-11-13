b65118f4e9c9db93507d0e41c2bfbf1e7b7132c1
yujeong <dnwjd812@gmail.com>
2025-11-14 02:00:09 +0900

    Title: 특정 조건 세 정수 비교, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 251114/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 251114/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,11 +1,11 @@
 a, b, c = map(int, input().split())
 
 min_num = min(a,b,c)
-
-if a == min_mun:
-    print('1')
-elif a != min_num:
-    print('0')
+# print(min_num)
+if a == min_num :
+    print('1',end=" ")
+elif a != min_num :
+    print('0', end=" ")
 
 if a == b == c :
     print('1')
