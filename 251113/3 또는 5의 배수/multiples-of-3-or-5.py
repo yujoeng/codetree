21858ca1931c5faf9190dc9d845d7a7bd321ff0c
yujeong <dnwjd812@gmail.com>
2025-11-13 21:42:13 +0900

    Title: 3 또는 5의 배수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251113/3 또는 5의 배수/multiples-of-3-or-5.py
@@ -0,0 +1,12 @@
+A = int(input())
+
+if A % 3 == 0 :
+    print('YES')
+elif A % 3 != 0:
+    print('NO')
+
+if A % 5 == 0 :
+    print('YES')
+elif A % 5 != 0:
+    # priprint('No')
+    print('NO')
