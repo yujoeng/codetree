69e776ec825ac5ac1f811df140f4e11efac257c0
yujeong <dnwjd812@gmail.com>
2025-11-14 01:39:16 +0900

    Title: 특정 조건 세 정수 비교, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251114/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+min_num = min(a,b,c)
+
+if a == min_mun:
+    print('1')
+elif a != min_num:
+    print('0')
+
+if a == b == c :
+    print('1')
+else:
+    print('0')
