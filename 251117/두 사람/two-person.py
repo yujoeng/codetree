411afe2e6055b001de5c1bfa7333b10480f24154
yujeong <dnwjd812@gmail.com>
2025-11-17 00:37:07 +0900

    Title: 두 사람, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251117/두 사람/two-person.py
+++ 251117/두 사람/two-person.py
@@ -10,7 +10,9 @@ gender2 = str(d)
 # 첫 번째 줄에 두 사람중 한 사람이라도 19세 이상이면서 남자라면 1을 출력합니다
 # 문제의 조건에 해당하지 않는다면 0을 출력합니다
 # print(age, sex)
-if age1 >= 19 and gender1 == 'M':
+if age1 >= 19 and gender1 == 'M':   # 사람1의 조건
+    print('1')
+elif age2 >= 19 and gender2 == 'M':   # 사람2의 조건
     print('1')
 else: 
     print('0')
