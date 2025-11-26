a144f508690a05105deee3d287a8158356d8bfb2
yujeong <dnwjd812@gmail.com>
2025-11-27 00:50:35 +0900

    Title: 일 수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251127/일 수 구하기/number-of-days-in-month.py
+++ 251127/일 수 구하기/number-of-days-in-month.py
@@ -14,9 +14,9 @@ month = [
     '11:30',
     '12:31']
 
-if n == 4 or n == 6 or n ==9 or n ==11 :
+if n == 4 or n == 6 or n == 9 or n ==11 :
     print('30')
-if n == 2:
+elif n == 2:
     print('28')
 else:
     print('31')
